727793e831a47441acdc1303f796f4664667be90
Ingmar Delsink <ingmardelsink@hotmail.com>
2017-11-18 15:09:13 +0100

    Add transform implementation to use git-describe as a transform

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const _ = require('lodash');
+const Transformtools = require('browserify-transform-tools');
+const GitDescribe = require('git-describe');
+
+const BrowserifyGitDescribe = Transformtools.makeRequireTransform('requireTransform',
+    {evaluateArguments: true},
+    (args, opts, cb) => {
+      if (_.first(args) === 'browserify-git-describe') {
+        let gitInfo = {};
+        try {
+          gitInfo = GitDescribe.gitDescribeSync(_.slice(args, 1));
+        } catch (err) {
+          console.log('Failed to retrieve git info:', err);
+        }
+        return cb(null, JSON.stringify(gitInfo));
+      } else {
+        return cb();
+      }
+    });
+
+module.exports = BrowserifyGitDescribe;
