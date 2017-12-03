7689996dff710a9b5dcd76e4074ca7216833e98c
Ingmar Delsink <ingmardelsink@hotmail.com>
2017-12-03 21:44:50 +0100

    Change lodash require statement to cherry-pick methods
    This for smaller browserify/rollup/webpack bundles.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const _ = require('lodash');
+const first = require('lodash/first');
+const slice = require('lodash/slice');
+
 const transformtools = require('browserify-transform-tools');
 const gitDescribeSync = require('git-describe').gitDescribeSync;
 const packageJson = require('./package.json');
@@ -8,10 +10,10 @@ const packageJson = require('./package.json');
 module.exports = transformtools.makeRequireTransform('requireTransform',
     {evaluateArguments: true},
     (args, opts, cb) => {
-      if (_.first(args) === packageJson.name) {
+      if (first(args) === packageJson.name) {
         let gitInfo = {};
         try {
-          gitInfo = gitDescribeSync.apply(this, _.slice(args, 1));
+          gitInfo = gitDescribeSync.apply(this, slice(args, 1));
         } catch (err) {
           console.log('Failed to retrieve git info:', err);
         }
